833b2925707bb72433260c5511b59bfc6ede1f1d
joppiesaus <job@function1.nl>
2015-04-17 16:27:01 +0200

    Added basic skeleton

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Blag</title>
+	<meta charset="utf-8">
+</head>
+<body>
 <php
 echo "TODO: Make blag";
 ?>
+</body>
+</html>
---
+++ post.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Post something</title>
+	<meta charset="utf-8">
+</head>
+<body>
+
+<input type="text" value="post_message" />
+<input type="submit" value="Post" />
+
+<php
+?>
+
+</body>
+</html>
