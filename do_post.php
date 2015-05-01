4500b3a245b73b3c965008ff9f75af2e62f4320a
joppiesaus <job@function1.nl>
2015-05-01 14:17:37 +0200

    Fixed files not working properly

--- do_post.php
+++ do_post.php
@@ -12,6 +12,13 @@ $date = getdate();
 $title = $_POST["p_title"];
 $text = $_POST["p_content"];
 
+// If post is empty, abort!
+if (strlen($text) === 0)
+{
+	echo "Post cannot be empty!";
+	exit;
+}
+
 #echo $date . "<br>";
 #echo $title . "<br>";
 #echo $text;
@@ -21,14 +28,17 @@ require_once "postcounter.php";
 // get a new post identification number
 $pid = getNewPostNumber();
 
-$postdir = "/p/" . $pid . "/";
+// Get the paths
+// You need DOCUMENT_ROOT. PHP doesn't allow that when mkdir
+$postdir = $_SERVER['DOCUMENT_ROOT'] . "/blag/p/" . $pid . "/";
 $postpath = $postdir . "index.html";
 
 // make a directory for the post
 mkdir($postdir);
 
+
 // save the post
-chmod($postpath, 0700); // Oh my god. PHP, y u do dis to me. we need to talk. its over. Oh, but wait, I need you,.
+// chmod($postpath, 0700); // Oh my god. PHP, y u do dis to me. we need to talk. its over. Oh, but wait, I need you,.
 $fs = fopen($postpath, "w");
 fwrite($fs, $text);
 fclose($fs);
@@ -38,7 +48,6 @@ $thing = fread($fs, filesize($postpath));
 fclose($fs);
 
 echo $thing;*/
-
 ?>
 
 </body>
